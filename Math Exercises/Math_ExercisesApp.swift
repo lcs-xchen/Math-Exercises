b3b8d84a5d72dc8b99b755ab80c75291b1d4ecd6
Cindy Chen <xchen@lcs.on.ca>
2024-01-15 11:30:23 -0500

    Initial Commit

---
+++ Math Exercises/Math_ExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_ExercisesApp.swift
+//  Math Exercises
+//
+//  Created by Cindy Chen on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct Math_ExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
