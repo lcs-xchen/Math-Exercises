eaa509348d7dff99c4a63df207644b90e6041134
Cindy Chen <xchen@lcs.on.ca>
2024-01-15 11:58:49 -0500

    I have completed the multiply section of the interface.

--- Math Exercises/ContentView.swift
+++ Math Exercises/ContentView.swift
@@ -8,17 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Stored properties
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 1
+    
+    // MARK: Computed properties
+    var times: Int {
+        return firstNumber * secondNumber
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            
+            VStack {
+                HStack{
+                    Spacer()
+                    Text("\(firstNumber)")
+                        .font(.system(size: 50))
+                        .padding()
+                    
+                }
+            }
+            
+            Stepper(value: $firstNumber, label: {Text("select first number")})
+                .padding()
+            
+           
+            HStack {
+                Image(systemName: "multiply")
+                    .font(.system(size: 50))
+                    .padding()
+                
+                Spacer()
+                
+                Text("\(secondNumber)")
+                    .font(.system(size: 50))
+                    .padding()
+            }
+            
+            Stepper(value: $secondNumber, label: {Text("select second number")})
+                .padding()
+                        
+         
+            
+            HStack {
+                Spacer()
+                Text("\(times)")
+                    .font(.system(size: 50))
+                    .padding()
+            }
+            
+         
         }
         .padding()
     }
 }
-
 #Preview {
-    ContentView()
+    TabView(selection: Binding.constant(3)){
+        
+        Text("Plus")
+            .tabItem {
+                Image(systemName: "plus")
+                Text("Plus")
+            }
+            .tag(1)
+            
+        
+        Text("Minus")
+            .tabItem {
+                Image(systemName: "minus")
+                Text("Minus")
+            }
+            .tag(2)
+            
+        
+        ContentView()
+            .tabItem {
+                Image(systemName: "multiply")
+                Text("Multiply")
+            }
+            .tag(3)
+        
+        Text("Divide")
+            .tabItem {
+                Image(systemName: "divide")
+                Text("Divide")
+            }
+            .tag(4)
+        
+        
+        
+    }
+    .accentColor(.purple)
 }
